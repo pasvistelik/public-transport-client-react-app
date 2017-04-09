d69140f3f99bea3f3b85e648c67c89be599966ec
pasvistelik <pasvistelik@gmail.com>
2017-04-09 18:09:17 +0300

    Delete unused files.

--- src/my_modules/apiConfig.js
+++
@@ -1,3 +0,0 @@
-//var apiPublicTransportServer = "http://adsghdfsjgdj-001-site1.gtempurl.com/api/";
-//var apiPublicTransportServer = "http://localhost:6967/api/";
-var apiPublicTransportServer = "https://ptp-97126.app.xervo.io/";
