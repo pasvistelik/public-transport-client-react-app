c29bcc0c52d878748cc7fb606314f2185d3773fb
pasvistelik <pasvistelik@gmail.com>
2017-12-20 23:56:04 +0300

    Update

--- src/page_components/FillWayInfoBlock.js
+++ src/page_components/FillWayInfoBlock.js
@@ -289,8 +289,6 @@ class FillWayInfoBlock extends Component {
                         <br/>
                         <div className="checkbox"><label className="checkbox_elem"><input name="isNeedCountingOnServer" checked={this.state.isNeedCountingOnServer} onChange={this.handleCountingOnServerFlagChanged} type="checkbox"/> Count ways on a server.</label></div>
                         
-                        <div className="form-group"><label>Момент отправки: <input name="time" type="time" value={this.state.selectedTime} onChange={this.handleSelectedTimeChanged}/></label></div>
-                        <div className="form-group"><label disabled>Успеть до: <input disabled name="time" type="time"/></label></div>
                         <div className="form-group"><label className="block_elem">Going speed: <input name="goingSpeed" type="range" min="1" max="10" step="0.5" value={this.state.selectedGoingSpeed} onChange={this.handleSelectedGoingSpeedChanged}/> {this.state.selectedGoingSpeed} км/ч</label></div>
                         <div className="form-group"><label className="block_elem">Reserved time: <input name="reservedTime" type="range" min="-6" max="6" step="0.5" value={this.state.selectedReservedTime} onChange={this.handleSelectedReservedTimeChanged}/> {this.state.selectedReservedTime} мин.</label></div>
                     </form>
